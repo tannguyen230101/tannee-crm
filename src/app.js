c669dbff5fbde45259ef1fc1c3c514ceccce9a8e
Nguyen Tan <nguyentan@MacBook-Air-cua-Nguyen-2.local>
2025-08-13 20:52:09 +0700

    test

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const connectMongo = require("./infrastructures/database/mongoClient");
 const errorHandler = require("./interfaces/http/middlewares/errorHandler");
-const Router = require('./interfaces/http/routes/index')
+const Router = require('./interfaces/http/routes/index');
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 connectMongo();
 
